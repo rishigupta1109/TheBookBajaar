510f18173ad7fd06866fc7edf17156bc7eb6bca6
Rishi <grishi634@gmail.com>
2022-06-05 21:46:59 +0530

    login signup completed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,41 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Login/Login.css";
 export default function Login() {
+  const [mode,setMode]=useState(0);    //0->register 1->login
   return (
     <div className="login-register">
       <div className="switch-btns">
-        <button>Register</button>
-        <button className="active">Login</button>
+        <button
+          className={mode ? "active" : undefined}
+          onClick={() => {
+            setMode(0);
+          }}
+        >
+          Register
+        </button>
+        <button
+          className={mode ? undefined : "active"}
+          onClick={() => {
+            setMode(1);
+          }}
+        >
+          Login
+        </button>
       </div>
       <div className="column form">
-        <h1 style={{alignSelf:"center"}}>Login</h1>
-        {/* <h2>Register</h2> */}
-        <div>
+        {mode===1&&<h1 style={{ alignSelf: "center" }}>Login</h1>}
+        {mode===0&&<h1 style={{ alignSelf: "center" }}>Register</h1>}
+        {mode=== 0&&<div>
           <label>First Name</label>
           <input type="text"></input>
-        </div>
-        <div>
+        </div>}
+        {mode===0&&<div>
           <label>Last Name</label>
           <input type="text"></input>
-        </div>
+        </div>}
         <div>
           <label>Email</label>
           <input type="text"></input>
         </div>
-        <div>
+       {mode==0&& <div>
           <label>College</label>
           <input type="text"></input>
-        </div>
+        </div>}
         <div>
           <label>Password</label>
           <input type="text"></input>
         </div>
-        <div>
+       {mode===0&& <div>
           <label>Confirm Password</label>
           <input type="text"></input>
-        </div>
-        <button>Register</button>
-        {/* <button>Login</button> */}
+        </div>}
+        {mode===0&&<button>Register</button>}
+       {mode===1&& <button>Login</button>}
       </div>
     </div>
   );
